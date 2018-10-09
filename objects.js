07e4d9b88c71be1b285844704fd01f86b00dcc77
Jane Amero <janie.amero@gmail.com>
2018-10-09 00:12:55 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,8 @@
+var playlist = {
+  celine: 'my heart will go on',
+  bono: 'love me',
+  dido: 'white flag',
+}
+function updatePlaylist(playlist, name, title) {
+  return Object.assign({}, playlist, {[key]: value })
+}
